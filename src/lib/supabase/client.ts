ea0d722040d0ffe93371f9a2c9847f2d5b89883a
허태규 <taegyu-heo@naver.com>
2025-09-30 06:55:39 +0000

    update

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,8 +1,4 @@
 // src/lib/supabase/client.ts
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
-export const createClient = () =>
-  createClientComponentClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
+export const createClient = () => createClientComponentClient()
